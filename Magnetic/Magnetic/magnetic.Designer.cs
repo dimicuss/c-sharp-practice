45596d82116634b15bdfc2d955a01993204ffdf4
Дмитрий Пятерников < >
2016-02-22 18:12:04 +0300

    update

--- Magnetic/Magnetic/magnetic.Designer.cs
+++ Magnetic/Magnetic/magnetic.Designer.cs
@@ -28,26 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button = new System.Windows.Forms.Button();
             this.drawField = new System.Windows.Forms.PictureBox();
-            this.pLeftX = new System.Windows.Forms.TextBox();
-            this.pLeftY = new System.Windows.Forms.TextBox();
-            this.pRightX = new System.Windows.Forms.TextBox();
-            this.pRightY = new System.Windows.Forms.TextBox();
             this.Space = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clearField = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.drawField)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button
-            // 
-            this.button.Location = new System.Drawing.Point(623, 418);
-            this.button.Margin = new System.Windows.Forms.Padding(2);
-            this.button.Name = "button";
-            this.button.Size = new System.Drawing.Size(56, 19);
-            this.button.TabIndex = 0;
-            this.button.Text = "Кнопка";
-            this.button.UseVisualStyleBackColor = true;
-            this.button.Click += new System.EventHandler(this.button_Click);
             // 
             // drawField
             // 
@@ -59,43 +45,34 @@
             this.drawField.Size = new System.Drawing.Size(400, 400);
             this.drawField.TabIndex = 1;
             this.drawField.TabStop = false;
-            // 
-            // pLeftX
-            // 
-            this.pLeftX.BackColor = System.Drawing.SystemColors.Window;
-            this.pLeftX.Location = new System.Drawing.Point(573, 24);
-            this.pLeftX.Name = "pLeftX";
-            this.pLeftX.Size = new System.Drawing.Size(44, 20);
-            this.pLeftX.TabIndex = 2;
-            // 
-            // pLeftY
-            // 
-            this.pLeftY.Location = new System.Drawing.Point(623, 24);
-            this.pLeftY.Name = "pLeftY";
-            this.pLeftY.Size = new System.Drawing.Size(44, 20);
-            this.pLeftY.TabIndex = 3;
-            // 
-            // pRightX
-            // 
-            this.pRightX.Location = new System.Drawing.Point(573, 50);
-            this.pRightX.Name = "pRightX";
-            this.pRightX.Size = new System.Drawing.Size(44, 20);
-            this.pRightX.TabIndex = 4;
-            // 
-            // pRightY
-            // 
-            this.pRightY.Location = new System.Drawing.Point(623, 50);
-            this.pRightY.Name = "pRightY";
-            this.pRightY.Size = new System.Drawing.Size(44, 20);
-            this.pRightY.TabIndex = 5;
+            this.drawField.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawField_MouseDown);
             // 
             // Space
             // 
-            this.Space.Location = new System.Drawing.Point(630, 100);
+            this.Space.Location = new System.Drawing.Point(638, 20);
             this.Space.Name = "Space";
             this.Space.Size = new System.Drawing.Size(37, 20);
             this.Space.TabIndex = 6;
             this.Space.TextChanged += new System.EventHandler(this.Space_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(435, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Введите интервал между пробелами:";
+            // 
+            // clearField
+            // 
+            this.clearField.Location = new System.Drawing.Point(538, 400);
+            this.clearField.Name = "clearField";
+            this.clearField.Size = new System.Drawing.Size(137, 23);
+            this.clearField.TabIndex = 8;
+            this.clearField.Text = "Очисить поле";
+            this.clearField.UseVisualStyleBackColor = true;
+            this.clearField.Click += new System.EventHandler(this.clearField_Click);
             // 
             // Magnetic
             // 
@@ -103,13 +80,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(690, 448);
+            this.Controls.Add(this.clearField);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Space);
-            this.Controls.Add(this.pRightY);
-            this.Controls.Add(this.pRightX);
-            this.Controls.Add(this.pLeftY);
-            this.Controls.Add(this.pLeftX);
             this.Controls.Add(this.drawField);
-            this.Controls.Add(this.button);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximumSize = new System.Drawing.Size(706, 487);
             this.MinimumSize = new System.Drawing.Size(706, 487);
@@ -122,14 +96,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button;
         private System.Windows.Forms.PictureBox drawField;
-        private System.Windows.Forms.TextBox pLeftX;
-        private System.Windows.Forms.TextBox pLeftY;
-        private System.Windows.Forms.TextBox pRightX;
-        private System.Windows.Forms.TextBox pRightY;
         private System.Windows.Forms.TextBox Space;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button clearField;
     }
 }
 
--- Magnetic/Magnetic/magnetic.cs
+++ Magnetic/Magnetic/magnetic.cs
@@ -4,7 +4,6 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
-using System.Drawing.Drawing2D;
 
 
 namespace Magnetic
@@ -12,48 +11,76 @@ namespace Magnetic
     public partial class Magnetic : Form
     {
         Graphics graph;
-        Field field;
+        Field field ;
+        List<Point> defectPoints;
+
 
         public Magnetic()
         {
             InitializeComponent();
             graph = Graphics.FromHwnd(drawField.Handle);
-            graph.SmoothingMode = SmoothingMode.AntiAlias;
             field = new Field(graph, drawField);
+            defectPoints = new List<Point>();
         }
 
-
-        private void button_Click(object sender, EventArgs e)
-        {
-            var pL = new Point(int.Parse(pLeftX.Text), int.Parse(pLeftY.Text));
-            var pR = new Point(int.Parse(pRightX.Text), int.Parse(pRightY.Text));
-            var pointSpace = int.Parse(Space.Text);
-
-
-            if (pL.X == pR.X)
-            {
-                MessageBox.Show("Неправильно выбраны координаты: X одной точки не должен равняться X другой.");
-            }
-            else if (pL.Y % pointSpace == 0 || pR.Y % pointSpace == 0)
-            {
-                MessageBox.Show("Неправильно выбраны координаты: Одна из точек находится на одной прямой с активной точкой.");
-            }
-            else if (pL.X > pR.X)
-            {
-                MessageBox.Show("Неправильно выбраны координаты: Точки дефектов пересекаются по вертикали.");
-            }
-            else field.drawWithDefects(pL, pR, pointSpace);
-        }
 
         private void Space_TextChanged(object sender, EventArgs e)
         {
             int pointSpace;
             int.TryParse(Space.Text, out pointSpace);
             
+            if (pointSpace != 0 && 400 % pointSpace != 0)
+            {
+                MessageBox.Show("Расстояние между активными точками должно быть кратным 400.");
+                return;
+            }
+
             if (pointSpace != 0)
             {
                 field.drawMarking(graph, pointSpace);
             }
+            else graph.Clear(Color.White);
+        }
+
+
+        private void drawField_MouseDown(object sender, MouseEventArgs e)
+        {
+            defectPoints.Add(new Point(e.X, e.Y));
+
+            if (defectPoints.Count == 2)
+            {
+                var pointSpace = int.Parse(Space.Text);
+
+                if (defectPoints[0].X == defectPoints[1].X)
+                {
+                    MessageBox.Show("Неправильно выбраны координаты: X одной точки не должен равняться X другой.");
+                }
+                else if (defectPoints[0].Y % pointSpace == 0 || defectPoints[1].Y % pointSpace == 0)
+                {
+                    MessageBox.Show("Неправильно выбраны координаты: Одна из точек находится на одной прямой с активной точкой.");
+                }
+                else if (defectPoints[0].X > defectPoints[1].X)
+                {
+                    MessageBox.Show("Неправильно выбраны координаты: Точки дефектов пересекаются по вертикали.");
+                }
+                else
+                {
+                    field.drawWithDefects(defectPoints[0], defectPoints[1], pointSpace);
+                    defectPoints.Clear();
+                }
+            }
+        }
+
+        private void clearField_Click(object sender, EventArgs e)
+        {
+            int pointSpace;
+            int.TryParse(Space.Text, out pointSpace);
+
+            if (pointSpace != 0)
+            {
+                field.drawMarking(graph, pointSpace);
+            }
+            else graph.Clear(Color.White);
         }
     }
 
@@ -65,7 +92,6 @@ namespace Magnetic
         private Graphics Graph;
         private PictureBox DrawField;
         private List<Point> Lst;
-
 
 
         public AkimaSpline (List<Point> lst, Graphics graph, PictureBox drawField)
@@ -81,6 +107,7 @@ namespace Magnetic
             return (int) alglib.spline1dcalc(c, xi);
         }
 
+
         private Point translateRotate(double oX, double oY, Point elm, int angle)
         {
             var X = elm.X + oX;
@@ -90,11 +117,13 @@ namespace Magnetic
                              (int) (X * Math.Sin(angle * Math.PI/180) + Y * Math.Cos(angle * Math.PI/180)));
         }
 
+
         private Point translateRotate(int angle, double oX, double oY, Point elm)
         {
             return new Point((int) (elm.X * Math.Cos(angle * Math.PI/180) - elm.Y * Math.Sin(angle * Math.PI/180) + oX),
                              (int) (elm.X * Math.Sin(angle * Math.PI/180) + elm.Y * Math.Cos(angle * Math.PI/180) + oY));
         }
+
 
         public void drawParabolicSpline()
         {
@@ -102,6 +131,7 @@ namespace Magnetic
             var oY = Lst[1].Y;
             var trnlLst = Lst.Select(elm => translateRotate(-oX, -oY, elm, 90)).ToList();
             List<Point> list = new List<Point>();
+            var pen = new Pen(Color.Black);
 
             alglib.spline1dbuildakima(trnlLst.Select(elm => (double) elm.X).ToArray(), 
                                       trnlLst.Select(elm => (double) elm.Y).ToArray(), 
@@ -112,13 +142,15 @@ namespace Magnetic
 
             var splined = list.Select(elm => translateRotate(-90, oX, oY, elm)).ToList();
 
-            for (int i = 0; i < splined.Count; i++)
-                Graph.FillRectangle(new Pen(Color.Black).Brush, splined[i].X - 1, splined[i].Y - 1, 1, 1);            
+            for (int i = 0; i < splined.Count - 1; i++)
+                Graph.DrawLine(pen, splined[i].X - 1, splined[i].Y - 1, splined[i + 1].X - 1, splined[i + 1].Y - 1);            
         }
+
 
         public void drawSpline()
         {
             var list = new List<Point>();
+            var pen = new Pen(Color.Black);
 
             alglib.spline1dbuildcubic(Lst.Select(elm => (double)elm.X).ToArray(),
                                       Lst.Select(elm => (double)elm.Y).ToArray(),
@@ -128,8 +160,8 @@ namespace Magnetic
             {
                 list.Add(new Point( i, returnY(i)));
             }
-            for (int i = 0; i < list.Count; i++)
-                Graph.FillRectangle(new Pen(Color.Black).Brush, list[i].X - 1, list[i].Y - 1, 1, 1);
+            for (int i = 0; i < list.Count - 1; i++)
+                Graph.DrawLine(pen, list[i].X - 1, list[i].Y - 1, list[i + 1].X - 1, list[i + 1].Y - 1);
         }
     }
 
@@ -168,9 +200,9 @@ namespace Magnetic
                 lstRight.Add(new Point(DrawField.Width, i));
             }
 
+
             indUp = (int) Math.Ceiling( (double)pointLeft.Y / pointSpace ) * pointSpace;
             indDown = (int) Math.Floor( (double)pointLeft.Y / pointSpace ) * pointSpace;
-
 
             defLeft.Add(new Point(0, indUp));
             defLeft.Add(pointLeft);
@@ -179,7 +211,6 @@ namespace Magnetic
 
             indUp = (int)Math.Ceiling( (double)pointRight.Y / pointSpace ) * pointSpace;
             indDown = (int)Math.Floor( (double)pointRight.Y / pointSpace ) * pointSpace;
-
 
             defRight.Add(new Point(DrawField.Width, indUp));
             defRight.Add(pointRight);
@@ -216,8 +247,12 @@ namespace Magnetic
             graph.Clear(Color.White);
 
             var pen = new Pen(Color.Gray);
+
+
             for (int i = -1; i <= DrawField.Height; i += pointSpace)
             {
+                graph.FillEllipse(pen.Brush, 0 - 4, i - 4, 8, 8);
+                graph.FillEllipse(pen.Brush, DrawField.Width - 7, i - 4, 8, 8);
                 graph.DrawLine(pen, 0, i, DrawField.Width, i);
                 graph.DrawLine(pen, i, 0, i, DrawField.Height);
             }
